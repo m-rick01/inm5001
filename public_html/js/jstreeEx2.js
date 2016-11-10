88459e1571e115cc639ef45290d8875b7d0dae13
Faysal87 <raddad.faysal@courrier.uqam.ca>
2016-11-10 16:44:41 -0500

    ajout de page CADRE, documents Coop et calandrier

--- public_html/js/jstreeEx2.js
+++ public_html/js/jstreeEx2.js
@@ -3,10 +3,18 @@
  */
 
 $(document).ready(function() {
-	$('div').eq(0).jstree();
+        $('#visionneuse').css("display", "none");
+	$('#ex1').eq(0).jstree();
 	$("#ex1 li").on("click", "a", function() {
 		//attachement traitement evénement click à la balise a
 		//voir http://api.jquery.com/on/
-		document.location.href = this;
+                if(($(this).hasClass('lien_visio'))) {
+                     console.log("cliqued!!");
+                    var url_lien = ($(this).attr('href'));
+                    $("#visionneuse").attr("src", url_lien);
+                    $('#visionneuse').css("display", "block");
+                }
+               
+                
 	});
 }); 
